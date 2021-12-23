04de628d964551b9870196794671e9c4d864a33c
Mohammed El Hassan <mohammedelhassan2001@gmail.com>
2021-12-23 16:21:20 +0200

    Home Page Design

--- index.php
+++ index.php
@@ -4,9 +4,26 @@
         
 <?php endif; ?>
 
-<h1>This is Home page</h1>
+<section class="home" id="home">
+    <div class="container">
+        <div class="column">
+            <div class="right">
+                <h1>A Simple Bookmark Manager</h1>
+                <p class="text">A clean and simple interface to organize your favourite websites. Open a new browser tab and see your sites load instantly. Try it for free.</p>
+                <div class="btns">
+                    <button>Get it on Chrome</button>
+                    <button>Get it on Firefox</button>
+                </div>
+            </div>
+            <div class="left">
+                <img src="/images/illustration-hero.svg" alt="hero">
+                <div class="shape"></div>
+            </div>
+        </div>
+    </div>
+</section>
 
-    
+
 
 <a class="whatsapp-btn" target="_blank" href="https://api.whatsapp.com/send?phone=+966555555228"><i class="fab fa-whatsapp"></i></a>
 
--- views/partials/header.php
+++ views/partials/header.php
@@ -10,16 +10,18 @@
     <script src="https://kit.fontawesome.com/88c35ab412.js" crossorigin="anonymous"></script>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700;800;900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="/styles/style.css">
+    <link rel="stylesheet" href="/styles/home-page.css">
     <title>Web Project</title>
 </head>
 
 
 <body>
     <header>
+        <img class="header-img" src="/images/logo-bookmark.svg" alt="logo">
         <ul>
             <li><a href="/index.php">Home</a></li>
-            <li><a href="/views/website/contact.php">Contact</a></li>
-            <li><a href="/views/website/register.php">Register</a></li>
+            <li><a href="/views/website/contact.php">Contact Us</a></li>
             <li><a href="/views/website/login.php">Login</a></li>
+            <a href="/views/website/register.php" class="header-btn">Register</a>
         </ul>
     </header>
