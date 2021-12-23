c589e1329a48048967e38d757b27992c2c735475
Mohammed El Hassan <mohammedelhassan2001@gmail.com>
2021-12-23 21:08:31 +0200

    Added Features Section

--- index.php
+++ index.php
@@ -8,11 +8,11 @@
     <div class="container">
         <div class="column">
             <div class="right">
-                <h1>A Simple Bookmark Manager</h1>
-                <p class="text">A clean and simple interface to organize your favourite websites. Open a new browser tab and see your sites load instantly. Try it for free.</p>
+                <h1>Inventory Management System</h1>
+                <p class="text">A clean and simple interface to organize your Products. Add, Delete, Edit Your Products,  Try it for free.</p>
                 <div class="btns">
-                    <button>Get it on Chrome</button>
-                    <button>Get it on Firefox</button>
+                    <button>Try It for Free</button>
+                    <button>Contact Us now</button>
                 </div>
             </div>
             <div class="left">
@@ -22,9 +22,56 @@
         </div>
     </div>
 </section>
+<section class="features" id="features">
+    <div class="container">
+        <h2 class="title">Features</h2>
+        <p class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, optio qui? Suscipit sequi numquam natus.</p>
+        <ul class="menu-btns">
+            <li data-clickedtab="1">Adding Products</li>
+            <li class="active" data-clickedtab="2">Speedy Searching</li>
+            <li data-clickedtab="3">Easy Sharing</li>
+        </ul>
+        <div data-contentnum="1" class="content">
+            <div class="column">
+                <div class="left">
+                    <img src="/images/illustration-features-tab-1.svg" alt="tab1">
+                </div>
+                <div class="right">
+                    <h2>Bookmark in one click</h2>
+                    <p class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem unde recusandae architecto reiciendis hic quaerat earum obcaecati, ea dolor? Eum!</p>
+                    <a href="">More Info</a>
+                </div>
+            </div>
+        </div>
+        <div data-contentnum="2"class="content active">
+            <div class="column">
+                <div class="left">
+                    <img src="/images/illustration-features-tab-2.svg" alt="tab1">
+                </div>
+                <div class="right">
+                    <h2>Editing Products</h2>
+                    <p class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem unde recusandae architecto reiciendis hic quaerat earum obcaecati, ea dolor? Eum!</p>
+                    <a href="">More Info</a>
+                </div>
+            </div>
+        </div>
+        <div data-contentnum="3" class="content">
+        <div class="column">
+                <div class="left">
+                    <img src="/images/illustration-features-tab-3.svg" alt="tab1">
+                </div>
+                <div class="right">
+                    <h2>Deleting Products</h2>
+                    <p class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem unde recusandae architecto reiciendis hic quaerat earum obcaecati, ea dolor? Eum!</p>
+                    <a href="">More Info</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 
 
 
-<a class="whatsapp-btn" target="_blank" href="https://api.whatsapp.com/send?phone=+966555555228"><i class="fab fa-whatsapp"></i></a>
+<a class="whatsapp-btn" target="_blank" href="https://api.whatsapp.com/send?phone=+201557817439"><i class="fab fa-whatsapp"></i></a>
 
 <?php include_once 'views/partials/footer.php' ?>
--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,36 @@
+// Whatsapp Button Logic
 const whatsappBtn = document.querySelector('.whatsapp-btn');
 let isStarted = false;
 window.onscroll = function(){
     if(window.scrollY > 500){
         if(!isStarted){
-            whatsappBtn.classList.toggle('show');
+            whatsappBtn.classList.add('show');
         }
-        isStarted = true;
+        // isStarted = true;
+    }else {
+        whatsappBtn.classList.remove('show');
     }
 }
+
+// Creating Tabs Login
+
+let Tabs = Array.from(document.querySelectorAll('.menu-btns li'));
+let contents = Array.from(document.querySelectorAll('.content'))
+
+Tabs.forEach(tab => {
+    tab.addEventListener('click',function(){
+        let tabSelected = tab.dataset.clickedtab;
+        contents.forEach(content=>{
+            content.classList.remove('active');  
+        })
+        Tabs.forEach(tab=>{
+            tab.classList.remove('active')
+        })
+        tab.classList.add('active');
+        contents.forEach(content=>{
+            if(content.dataset.contentnum === tabSelected){
+                content.classList.add('active')
+            }
+        })
+    });
+});
